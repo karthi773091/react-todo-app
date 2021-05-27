d9bb0802900fb74dd35a3c5a76c22035b0113efc
Karthi1999 <krishnakalai1999@gmail.com>
2021-05-27 22:20:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="ToDo-App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
